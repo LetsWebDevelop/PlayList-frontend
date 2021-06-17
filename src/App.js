e58660f0d92d0fbee4c8143ff37d1eaa3a086f25
Flipmemax <maxphoogland@gmail.com>
2021-06-17 15:02:00 +0200

    add signup page, fixed navbar button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Switch>
+        <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route path="/" component={HomePage} exact />
       </Switch>
