ae29aafc6f74c44e8c5708a7e6a0591504c0a8f6
Flipmemax <maxphoogland@gmail.com>
2021-06-18 17:58:20 +0200

    add fetchPlaylists - stored them in redux

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,10 +3,12 @@ import appState from "./appState/reducer";
 import user from "./user/reducer";
 import spotifyToken from "./spotifyToken/reducer";
 import newPlaylist from "./createPlaylist/reducer";
+import playlists from "./getPlaylists/reducer";
 
 export default combineReducers({
   appState,
   user,
   spotifyToken,
   newPlaylist,
+  playlists,
 });
