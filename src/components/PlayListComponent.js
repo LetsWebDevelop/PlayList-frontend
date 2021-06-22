bc8edb34dd026c76248640eceb4e6f4546bffedf
Flipmemax <maxphoogland@gmail.com>
2021-06-23 01:35:10 +0200

    songs in playlists are now being rendered -- also started working on getting rid of the inline styling and replacing it with css files

--- src/components/PlayListComponent.js
+++ src/components/PlayListComponent.js
@@ -1,23 +1,46 @@
 import { selectPlaylistByID } from "../store/PlaylistByID/selectors";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { clearSpotifyMusic } from "../store/spotifyMusic/actions";
 
+import "./MusicComponent.css"
+import MusicComponent from "./MusicComponent"
+import { setSpotifySong } from "../store/playSong/actions";
+
 export default function PlayListComponent() {
+  const [uri, setUri] = useState("")
   const playlist = useSelector(selectPlaylistByID);
+  const songs = playlist.songs
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(clearSpotifyMusic())
+    dispatch(setSpotifySong(uri))
+    console.log("check:", playlist)
   })
 
   return (
-    <div>
-      <p>
-        {playlist.id}
-        {" "}
-        {playlist.name}
-      </p>
+        <div className="mainBox">
+        {songs?.map((song) => {
+          return (
+            <div key={song.id}>
+              <div className="musicBox">
+                <div onClick={() => setUri(song.uri)} className="playSong">
+                  <MusicComponent img={song.image} />
+                </div>
+                <div className="songTitleArtistBox">
+                  <div className="defaultTitleText">
+                    <MusicComponent title={song.title} />
+                  </div>
+                  <div className="defaultArtistText">
+                    <MusicComponent artist={song.artist} />
+                  </div>
+              </div> 
+            </div>
+          </div>
+      )
+    })}
     </div>
+   
   );
 }
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -18,24 +18,32 @@ const image = props.track.album.images[2].url;
 const uri = props.track.uri;
 const origin = "Spotify";
 
+function Succes(playlist) {
+  alert(`${title} - successfully added to "${playlist}"`)
+}
+
   return ( 
       <div className="popup-box">
         <div className="box">
         <span className="close-icon" onClick={props.handleClose}>x</span>
         <div>
-           <h3 style={{ color: "green"}}>
+           <h3 className="green">
              Select Playlist to add 
-             </h3> 
-          <p style={{fontWeight: "bold"}}>
-            {props.track.name}
-          </p>
+             </h3>            
+              
+          <h3 className="grey">
+           {artist} - <span className="title"> {props.track.name} </span>
+          </h3>
           </div>
         {playlists.map((playlist) => {
         return (
           <div key={playlist.id} >
-         <div className="selectPlaylist" onClick={() => 
-          dispatch(addSong(title, artist, image, uri, origin, playlist.id ))}>
-         <p>{playlist.name} {playlist.id}</p>
+         <div className="selectPlaylist" onClick={() => {
+           dispatch(addSong(title, artist, image, uri, origin, playlist.id ));
+           Succes(playlist.name);
+          }
+          }>
+         <p>{playlist.name}</p>
          </div>
         </div>
         )
