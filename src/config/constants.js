d656ed8f93c26efb9f36faa795d1716a61e4f4f5
Flipmemax <maxphoogland@gmail.com>
2021-07-10 20:09:18 +0200

    edit ApiUrl

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl =
-  "https://playlist99.herokuapp.com/" || "http://localhost:4000";
+  "https://playlist99.herokuapp.com" || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
